fbb2d9842dd742f49897851b2d8d0c947d213543
Joshua-McGee <jcooler.cooler@gmail.com>
2019-09-16 23:38:58 +0000

    finished exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,5 +3,15 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
+puts Store.ids.inspect
+
+@store1 = Store.find_by(id: 1)
+@store1.annual_revenue = 4000000
+@store1.save
+
+@store2 = Store.find_by(id: 2)
+
+p @store1
+p @store2
 
 # Your code goes here ...
