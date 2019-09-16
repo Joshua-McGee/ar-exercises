610cf9064c7fe679224ac5658e12137149af4fff
Joshua-McGee <jcooler.cooler@gmail.com>
2019-09-16 23:16:49 +0000

    finished exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,29 @@
 require_relative '../setup'
 
+# Store 1
+burnaby = Store.new
+burnaby.annual_revenue = 300000
+burnaby.mens_apparel = true
+burnaby.womens_apparel = true
+burnaby.save
+# Store 2
+richmond = Store.new
+richmond.annual_revenue = 31260000
+richmond.mens_apparel = false
+richmond.womens_apparel = true
+richmond.save
+# Store 3
+gastown = Store.new
+gastown.annual_revenue = 190000
+gastown.mens_apparel = true
+gastown.womens_apparel = false
+gastown.save
+
+
+
 puts "Exercise 1"
 puts "----------"
+# put the number of stores in the database
+puts Store.count
 
 # Your code goes below here ...
---
+++ setupData.md
@@ -0,0 +1,4 @@
+# to set up your database and remove the pg error run this
+bundle exec ruby (<your file here>)
+  example: bundle exec ruby exercises/exercise_1.rb
+  
